0546e25a5fef210050ab6035796fa2566ec05d83
Nishit Soni <soni.nishit@successive.tech>
2022-07-28 23:20:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { MuiTable } from "../src/component/mui-table/index";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+   <MuiTable />
+    </div>
+  );
+}
+
+export default App;
